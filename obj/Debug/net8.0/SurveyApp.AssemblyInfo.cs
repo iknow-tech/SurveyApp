ee09901e0af900de45cd33cfc90d96b137a5d2bb
Burak Güney <burakguney@outlook.com.tr>
2024-03-27 23:52:04 +0300

    bugfix and gitignore

--- obj/Debug/net8.0/SurveyApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/SurveyApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SurveyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+322e4d5cd2f63f31b3c806fc0337f4b58d43a999")]
 [assembly: System.Reflection.AssemblyProductAttribute("SurveyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SurveyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
